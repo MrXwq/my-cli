d912f18671f52189637b775580dfd57f74923033
MrXwq <MrXwq@users.noreply.github.com>
2021-10-18 13:10:15 +0000

    Update webpack5 + Vue3.x + TypeScript Starter 21b57f84ff5da7f6a1e7b297877d640842473692

--- src/ajax/apis.ts
+++
@@ -1,9 +0,0 @@
-import { apiPost, apiGet, apiDelete, apiPut } from './apiMethods'
-
-export const postUser = (params?: any) => apiPost('/user', params)
-
-export const getUser = (params?: any) => apiGet('/user', params)
-
-export const apiLogin2 = (params?: any) => apiPut('/auth', params)
-
-export const apiLogin3 = () => apiDelete('/auth')
